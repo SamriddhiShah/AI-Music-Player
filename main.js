3a578b09d2b318a9b147254f7cbbfc089be0448c
SamriddhiShah <70637123+SamriddhiShah@users.noreply.github.com>
2020-09-09 16:09:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,36 +1,80 @@
-function setup(){
-    canvas= createCanvas(300,300);
-    canvas.position(450,257);
-    classifier=ml5.imageClassifier('MobileNet', modelLoaded);
-    video=createCapture(VIDEO);
-    video.hide();
 
-    
+leftWristY = 0;
+
+rightWristY = 0;
+
+
+volume = 0;
+
+function setup() {
+    canvas = createCanvas(400, 400);
+    canvas.center();
+    video = createCapture(VIDEO);
+video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded(){
-    console.log('Modal has Successfully loaded!!');
+function draw() {
+    image(video, 0, 0, 400, 400);
+    fill('#F87217');
+    stroke('#F87217');
+   
+
+ if(leftWristY> rightWristY){
+chosen="find";
+play();
+ }
+ else{
+     chosen="not-find";
+     play();
+ }
+
+}
+
+
+
+song = "";
+music = "";
+
+function preload() {
+    song = loadSound("move.mp3");
+    music=loadSound("find.mp3");
+
+}
+
+function play() {
+    if (chosen == "find") {
+        song.play();
+        document.getElementById("song-name").innerHTML=" Find Yourself in The Song";
+    } else {
+        music.play();
+        
+        document.getElementById("song-name").innerHTML=" Get Moving";
+    }
 
 
 }
 
-function draw(){
-    image(video,0,0,300,200);
-    classifier.classify(video,gotResult);
-    
+function modelLoaded() {
+    console.log("MODEL LOADED! YIPEE!");
 }
 
-function preload(){
-}
+function gotPoses(result) {
+    if (result.length > 0) {
+        console.log(result);
+      
+        leftWristY = result[0].pose.leftWrist.y;
 
-function gotResult(error, result){
-    if (error){
-        console.log(error);
+        rightWristX = result[0].pose.rightWrist.x;
+        
+
+     
+        console.log(scoreLeftWrist);
+
+        console.log("l wrist= " + leftWristX + "  " + leftWristY);
+        console.log("r wrist= " + rightWristX + "  " + rightWristY);
 
     }
-    else{
-        console.log(result[0].label);
-        document.getElementById("obj_result").innerHTML=result[0].label;
-        document.getElementById("acc_result").innerHTML=result[0].confidence.toFixed(3);
-    }
+
 }
